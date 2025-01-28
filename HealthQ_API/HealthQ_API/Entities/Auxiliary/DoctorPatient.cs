1d7d85017cf8e3df5c6e02f4e01b31f4b460e8a4
Ankmit <akmitliviy@gmail.com>
2025-01-28 16:39:36 +0200

    feat: new doctor patient table (#10)

---
+++ HealthQ_API/HealthQ_API/Entities/Auxiliary/DoctorPatient.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthQ_API.Entities.Auxiliary;
+
+[Table("doctor_patient", Schema = "public")]
+public class DoctorPatient
+{
+    [MaxLength(254)]
+    public string? DoctorEmail { get; set; }
+    
+    [MaxLength(254)]
+    public string? PatientEmail { get; set; }
+    
+    public required UserModel Doctor { get; set; }
+    
+    public required UserModel Patient { get; set; }
+}
