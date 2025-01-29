cb69f983285256bd9bebdb74ac10e89437bee3f6
Ankmit <akmitliviy@gmail.com>
2025-01-29 22:38:24 +0200

    Feature/doctor page (#13)
    * fix: (API) remove hardcoded section in questionnaire
    
    * feat: add sidebar
    
    * feat: new doctor patient table (#10)
    
    * fix: (API) remove hardcoded section in questionnaire
    
    * feat: implement doctor's templates page
    
    * feat: reformed db
    
    * bugFix: DB many-to-mant tables' structure fix
    
    * feat: update and implement api calls regarding doctor's questionnaires
    
    * feat: implement patients retrieve from API
    
    ---------
    
    Co-authored-by: KOMPUTER\Danatello <chotkiydanik@gmail.com>

--- HealthQ_API/HealthQ_API/obj/Debug/net9.0/HealthQ_API.AssemblyInfo.cs
+++ HealthQ_API/HealthQ_API/obj/Debug/net9.0/HealthQ_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HealthQ_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de270f14bf3b7cc3726398874a345a482a880fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6980fb446af4616005a90d717bc1237050a7956b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HealthQ_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HealthQ_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
